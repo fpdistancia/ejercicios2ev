189703b2a6257e0e6ada935749738c36b5474f91
Julio Pedro López Bayón <jplbayon@gmail.com>
2021-03-10 16:11:46 +0100

    Modificación en PruebaPublicaciones

--- src/unidad7/PruebaPublicaciones.java
+++ src/unidad7/PruebaPublicaciones.java
@@ -7,11 +7,14 @@ public class PruebaPublicaciones {
 	public static void main(String[] args) {
 		ArrayList<Publicación> publicaciones = new ArrayList<>();
 		
-		publicaciones.add(new Libro(1, "El Quijote", 1605, "Cervantes"));
-		publicaciones.add(new Revista(2, "x", 1, 1, 2021, 100));
+		procesarPublicación(new Libro(1, "El Quijote", 1605, "Cervantes"));
+		procesarPublicación(new Revista(2, "x", 1, 1, 2021, 100));
 		
-		for (Publicación p: publicaciones)
-			System.out.println(p.toString());
+		
+	}
+	
+	static void procesarPublicación(Publicación p) {
+		System.out.println(p.toString());
 	}
 
 }
